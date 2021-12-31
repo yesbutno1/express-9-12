0d70e4861211ad80bbb8dbaed7ecb96b137605ef
Eleonora <eleonora1weber@gmail.com>
2021-12-31 19:22:54 +0100

    setup

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-const connection = require('./db-config');
 const express = require('express');
 const app = express();
 //const Joi = require('joi');
 const { setupRoutes } = require("./routes/index")
 
 const port = process.env.PORT || 3000;
-
-
-connection.connect((err) => {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-  } else {
-    console.log('connected as id ' + connection.threadId);
-  }
-});
 
 app.use(express.json());
 
